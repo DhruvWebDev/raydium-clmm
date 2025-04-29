cb1e441ddbdd2ec208f46b80ed6a0c11b7fa0eec
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-04-30 00:38:17 +0530

    Update system.rs

--- programs/amm/src/util/system.rs
+++ programs/amm/src/util/system.rs
@@ -1,5 +1,6 @@
 use anchor_lang::{prelude::*, system_program};
 
+//TODO TO UNDERSTAND THIS FN()
 pub fn create_or_allocate_account<'a>(
     program_id: &Pubkey,
     payer: AccountInfo<'a>,
@@ -12,11 +13,14 @@ pub fn create_or_allocate_account<'a>(
     let current_lamports = target_account.lamports();
 
     if current_lamports == 0 {
+        //min balance for rent exemption
         let lamports = rent.minimum_balance(space);
+        //cpi_accounts are the accounts that are involved in the cpi call
         let cpi_accounts = system_program::CreateAccount {
             from: payer,
             to: target_account.clone(),
         };
+        //cpi_ctx contains the program id and the accounts
         let cpi_context = CpiContext::new(system_program.clone(), cpi_accounts);
         system_program::create_account(
             cpi_context.with_signer(&[siger_seed]),
@@ -25,10 +29,12 @@ pub fn create_or_allocate_account<'a>(
             program_id,
         )?;
     } else {
+        //here we find the min. lamports for thsi account sie for rent exemption
         let required_lamports = rent
             .minimum_balance(space)
             .max(1)
             .saturating_sub(current_lamports);
+        // if required lamports are greater than zero we would transfer some sol to make it rent expempt
         if required_lamports > 0 {
             let cpi_accounts = system_program::Transfer {
                 from: payer.to_account_info(),
@@ -37,6 +43,7 @@ pub fn create_or_allocate_account<'a>(
             let cpi_context = CpiContext::new(system_program.clone(), cpi_accounts);
             system_program::transfer(cpi_context, required_lamports)?;
         }
+        //now we allocate this account space to the target_acc
         let cpi_accounts = system_program::Allocate {
             account_to_allocate: target_account.clone(),
         };
