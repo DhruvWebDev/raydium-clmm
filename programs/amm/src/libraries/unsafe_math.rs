7f6c1b9844f0a2e7b86ba0af1f7bcc13ca2c61b9
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-05-14 19:21:59 +0530

    Update unsafe_math.rs

--- programs/amm/src/libraries/unsafe_math.rs
+++ programs/amm/src/libraries/unsafe_math.rs
@@ -10,6 +10,10 @@ pub trait UnsafeMathTrait {
 }
 
 impl UnsafeMathTrait for u64 {
+
+    //x % y returns the remainder then we apply a check whether it is grester or smaller with returns booleans.
+    //we convert the bool into u64(unsigned integer).
+    //if true than 1 and if false than 0
     fn div_rounding_up(x: Self, y: Self) -> Self {
         x / y + ((x % y > 0) as u64)
     }
