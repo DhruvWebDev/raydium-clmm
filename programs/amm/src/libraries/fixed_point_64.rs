33392899b3e50d9e574e0e7aec74e6a33d57b0a0
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-05-24 19:48:25 +0530

    Update fixed_point_64.rs

--- programs/amm/src/libraries/fixed_point_64.rs
+++ programs/amm/src/libraries/fixed_point_64.rs
@@ -1,5 +1,6 @@
 /// A library for handling Q64.64 fixed point numbers
 /// Used in sqrt_price_math.rs and liquidity_amounts.rs
 
+//u64 is 2^64 - 1
 pub const Q64: u128 = (u64::MAX as u128) + 1; // 2^64
 pub const RESOLUTION: u8 = 64;
